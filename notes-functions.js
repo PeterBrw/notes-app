d6d2528aa14f019101140121398cbcc834c96b30
Petru <peter.giurgiu@gmail.com>
2019-02-19 22:22:06 +0200

    70-checkbox-challenges

--- notes-functions.js
+++ notes-functions.js
@@ -28,8 +28,9 @@ const removeNote = function (id) {
 // Generate the DOM structure for a note
 const generateNoteDOM = function(note) {
   const noteEl = document.createElement('div')
-  const textEl = document.createElement('span')
+  const textEl = document.createElement('a')
   const button = document.createElement('button')
+  const anchor = document.createElement('a')
 
   // Setup the remove note button
   button.textContent = 'x'
@@ -46,8 +47,9 @@ const generateNoteDOM = function(note) {
   } else {
     textEl.textContent = 'Unnamed note'
   }
+  textEl.setAttribute('href', `/edit.html#${note.id}`)
   noteEl.appendChild(textEl)
-
+  
   return noteEl
 }
 
