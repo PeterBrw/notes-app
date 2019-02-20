775008dad2115538f8a4f094e25add9525a10ede
Petru <peter.giurgiu@gmail.com>
2019-02-20 18:46:01 +0200

    76-integrating-dates-part-i

--- notes.js
+++ notes.js
@@ -8,11 +8,13 @@ renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function(e) {
   const id = uuidv4()
-  
+  const timestamp = moment().valueOf()
   notes.push({
     id: id,
     title: '',
-    body: ''
+    body: '',
+    createdAt: timestamp,
+    updatedAt: timestamp
   })
   saveNotes(notes)
   location.assign(`/edit.html#${id}`)
@@ -34,13 +36,3 @@ window.addEventListener('storage', function(e) {
   }
 })
 
-// const now = moment()
-// now.subtract(1, 'week').subtract(20, 'days')
-// console.log(now.format('MMMM Do YYYY'))
-// console.log(now.fromNow())
-// const nowTimestamp = now.valueOf()
-
-// console.log(moment(nowTimestamp).toString())
-
-const birthday = moment().set({'year': 1992, 'month': 5, 'date': 8})
-console.log(birthday.format('MMM D YYYY'))
