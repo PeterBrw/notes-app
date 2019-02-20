5724c76444dead24e74a54fad02f28eb9096ff60
Petru <peter.giurgiu@gmail.com>
2019-02-20 16:22:30 +0200

    73-syncing-data-across-pages

--- notes-edit.js
+++ notes-edit.js
@@ -1,30 +1,47 @@
-const noteId = location.hash.substr(1)
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
+const noteId = location.hash.substring(1)
 let notes = getSavedNotes()
-const note = notes.find(function(note) {
-  return note.id === noteId
+let note = notes.find(function (note) {
+    return note.id === noteId
 })
 
-if(note === undefined) {
-  location.assign('/index.html')
+if (note === undefined) {
+    location.assign('/index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('#note-body').value = note.body
+titleElement.value = note.title
+bodyElement.value = note.body
 
-document.querySelector('#note-title').addEventListener('input', function(e) {
+titleElement.addEventListener('input', function (e) {
     note.title = e.target.value
     saveNotes(notes)
 })
 
-document.querySelector('#note-body').addEventListener('input', function(e) {
-  note.body = e.target.value
-  saveNotes(notes)
+bodyElement.addEventListener('input', function (e) {
+    note.body = e.target.value
+    saveNotes(notes)
 })
 
-document.querySelector('#remove-note').addEventListener('click', function(e) {
-  notes = notes.filter(function(note) {
-    return note.id !== noteId
-  })
-  saveNotes(notes)
-  location.assign('/index.html')
+removeElement.addEventListener('click', function (e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+window.addEventListener('storage', function(e) {
+  if(e.key === 'notes') {
+    notes = JSON.parse(e.newValue)
+    let note = notes.find(function (note) {
+      return note.id === noteId
+    })
+    
+    if (note === undefined) {
+        location.assign('/index.html')
+    }
+    
+    titleElement.value = note.title
+    bodyElement.value = note.body
+  }
 })
